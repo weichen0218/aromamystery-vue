3a8d780122e8a9df3077d47f7d651efdf9187226
winnie19970218 <winnie19970218@gmail.com>
2023-11-08 17:40:03 +0800

    deploy

---
+++ assets/statusStore-caf546f2.js
@@ -0,0 +1,1 @@
+import{d as o}from"./index-8e8231f6.js";const n=o("statusStore",{state:()=>({isLoading:!1,cartLoadingItem:"",messages:[]}),actions:{pushMessage(s){const{style:e,title:t,content:a}=s;this.messages.push({style:e,title:t,content:a})},resetMessage(){this.messages=[]}}});export{n as s};
