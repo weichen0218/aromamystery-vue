60a635f01236f280072d34301344a299a2de5005
winnie19970218 <winnie19970218@gmail.com>
2023-11-12 19:21:38 +0800

    deploy

---
+++ assets/statusStore-cf1f3c97.js
@@ -0,0 +1,1 @@
+import{d as o}from"./index-09ee44b4.js";const n=o("statusStore",{state:()=>({isLoading:!1,cartLoadingItem:"",messages:[]}),actions:{pushMessage(s){const{style:e,title:t,content:a}=s;this.messages.push({style:e,title:t,content:a})},resetMessage(){this.messages=[]}}});export{n as s};
