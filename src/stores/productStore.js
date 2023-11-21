e6fdd9c26e3e2ff1a56451a048ef0b8b13892c96
weichen0218 <iamweichen0218@gmail.com>
2023-11-21 20:33:51 +0800

    add pagination

--- src/stores/productStore.js
+++ src/stores/productStore.js
@@ -5,6 +5,7 @@ const status = statusStore()
 export default defineStore('productStore', {
   state: () => ({
     products: [],
+    pagination: {},
     product: {},
     category: []
   }),
@@ -17,7 +18,6 @@ export default defineStore('productStore', {
         const arry = this.products.map((item) => item.category)
         const newSet = new Set(arry)
         this.category = [...newSet]
-        // console.log(this.category)
         status.isLoading = false
       })
     },
@@ -26,7 +26,15 @@ export default defineStore('productStore', {
       status.isLoading = true
       axios.get(api).then((res) => {
         this.product = res.data.product
-        // console.log(this.product)
+        status.isLoading = false
+      })
+    },
+    getProducts(page = 1) {
+      const api = `${import.meta.env.VITE_APP_API}/api/${import.meta.env.VITE_APP_PATH}/products?page=${page}`
+      status.isLoading = true
+      axios.get(api).then((res) => {
+        this.products = res.data.products
+        this.pagination = res.data.pagination
         status.isLoading = false
       })
     }
--- src/views/front/Products.vue
+++ src/views/front/Products.vue
@@ -1,7 +1,21 @@
 <template>
   <Loading :active="isLoading"></Loading>
   <section class="container py-5" id="product">
-    <h2 class="text-center mb-4">所有課程</h2>
+    <h2 class="text-center mb-4">精選課程</h2>
+    <ul class="nav nav-pills justify-content-center mb-4">
+      <li class="nav-item mx-2">
+        <a class="nav-link active" aria-current="page" href="#">全部</a>
+      </li>
+      <li class="nav-item mx-2">
+        <a class="nav-link" href="#">初階</a>
+      </li>
+      <li class="nav-item mx-2">
+        <a class="nav-link" href="#">中階</a>
+      </li>
+      <li class="nav-item mx-2">
+        <a class="nav-link" href="#">進階</a>
+      </li>
+    </ul>
     <div class="row g-4 pb-5">
       <price-card
         v-for="(product, index) in sortProducts"
@@ -15,30 +29,34 @@
         :origin_price="product.origin_price"
       />
     </div>
+    <pagination :pages="pagination" @emit-pages="getProducts"></pagination>
   </section>
 </template>
 <script>
 import PriceCard from '@/components/PriceCard.vue'
+import Pagination from '@/components/Pagination.vue'
 import productStore from '@/stores/productStore.js'
 import statusStore from '@/stores/statusStore.js'
 import { mapState, mapActions } from 'pinia'
 import gsap from 'gsap'
 export default {
   components: {
-    'price-card': PriceCard
+    'price-card': PriceCard,
+    pagination: Pagination
   },
   data() {
     return {}
   },
   computed: {
     ...mapState(statusStore, ['isLoading']),
-    ...mapState(productStore, ['sortProducts'])
+    ...mapState(productStore, ['sortProducts', 'pagination'])
   },
   methods: {
-    ...mapActions(productStore, ['getAllProducts'])
+    ...mapActions(productStore, ['getAllProducts', 'getProducts'])
   },
   created() {
-    this.getAllProducts()
+    // this.getAllProducts()
+    this.getProducts()
   },
   mounted() {
     gsap.set('#product', { opacity: 0 })
